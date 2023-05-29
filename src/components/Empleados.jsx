4274a68a2214a72666eea1d100774b6da7b1512e
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-29 10:42:42 -0300

    se agrega imagenes de una url porque las locales no las toma

--- src/components/Empleados.jsx
+++ src/components/Empleados.jsx
@@ -1,22 +1,22 @@
 let Empleados = [
     { id: 1, fullName: "Laya Dueñas", title: "CEO", department: "Business",
-    pic: "https://xsgames.co/randomusers/avatar.php?g=female" },
+    pic: "https://xsgames.co/randomusers/assets/avatars/female/22.jpg" },
     { id: 2, fullName: "Astryd Vallés", title: "CMO", department:
-    "Marketing", pic: "https://xsgames.co/randomusers/avatar.php?g=male" },
+    "Marketing", pic: "https://xsgames.co/randomusers/assets/avatars/male/42.jpg" },
     { id: 3, fullName: "Shantell Meza", title: "CFO", department:
-    "Business", pic: "https://xsgames.co/randomusers/avatar.php?g=female" },
+    "Business", pic: "https://xsgames.co/randomusers/assets/avatars/female/58.jpg" },
     { id: 4, fullName: "Sergio Ocampo", title: "CTO", department:
-    "Engineering", pic: "https://xsgames.co/randomusers/avatar.php?g=male" },
+    "Engineering", pic: "https://xsgames.co/randomusers/assets/avatars/male/73.jpg" },
     { id: 5, fullName: "Ares Jiménez", title: "Art Director", department:
-    "Marketing", pic: "https://xsgames.co/randomusers/avatar.php?g=male" },
+    "Marketing", pic: "https://xsgames.co/randomusers/assets/avatars/male/67.jpg" },
     { id: 6, fullName: "Marta Pérez", title: "Frontend Dev", department:
-    "Engineering", pic: "https://xsgames.co/randomusers/avatar.php?g=female" },
+    "Engineering", pic: "https://xsgames.co/randomusers/assets/avatars/female/77.jpg" },
     { id: 7, fullName: "Ellen Balderas", title: "Digital Strategist",
-    department: "Marketing", pic: "https://xsgames.co/randomusers/avatar.php?g=female" },
+    department: "Marketing", pic: "https://xsgames.co/randomusers/assets/avatars/female/32.jpg" },
     { id: 8, fullName: "Cynthia Valentín", title: "Backend Dev", department:
-    "Engineering", pic: "https://xsgames.co/randomusers/avatar.php?g=female" },
+    "Engineering", pic: "https://xsgames.co/randomusers/assets/avatars/female/3.jpg" },
     { id: 9, fullName: "Bernard Jung", title: "DevOps Engineer", department:
-    "Engineering", pic: "https://xsgames.co/randomusers/avatar.php?g=male" },
+    "Engineering", pic: "https://xsgames.co/randomusers/assets/avatars/male/11.jpg" },
     ]
     
     export default Empleados;
